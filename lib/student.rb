d5a8a96dfb2d950cf26b0fffca15a2029ad503bb
Jenise Ruehle <jela07@gmail.com>
2020-08-05 15:05:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,11 @@ class Student
     end     
   end 
   
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(X)
+    sql = <<-SQL 
+      SELECT *
+      FROM students
+      
     
   end 
   
