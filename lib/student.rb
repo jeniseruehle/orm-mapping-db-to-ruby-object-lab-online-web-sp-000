5d2e159902fa386df111faee9ee6bb9bf6efb0de
Jenise Ruehle <jela07@gmail.com>
2020-08-05 15:00:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,17 @@ class Student
     end.first
   end
   
-  def self.
+  def self..all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
+  end 
   
   def save
     sql = <<-SQL
